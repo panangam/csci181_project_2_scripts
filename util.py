1b4203c42827b8664941c0821186e12e8c8851a2
Oras Phongpanangam <panangam@gmail.com>
2018-05-22 01:06:21 -0700

    add credit for vividict

--- util.py
+++ util.py
@@ -5,6 +5,7 @@ Created on Mon May 21 22:16:36 2018
 @author: Panangam
 """
 
+# taken from https://stackoverflow.com/questions/635483/what-is-the-best-way-to-implement-nested-dictionaries
 class Vividict(dict):
     def __missing__(self, key):
         value = self[key] = type(self)() # retain local pointer to value
